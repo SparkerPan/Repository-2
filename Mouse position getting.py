ee5bd159f9ad24296857e3dd7f8aa5c8fb5ee0d1
SparkerPan <36907912+SparkerPan@users.noreply.github.com>
2018-08-16 16:13:05 +0800

    Update Mouse position getting.py

--- Mouse position getting.py
+++ Mouse position getting.py
@@ -10,32 +10,7 @@ try:
         posStr = "Position:" + str(x).rjust(4)+','+str(y).rjust(4)
         print(posStr) ###		
         time.sleep(0.5)
-        os.system('cls')   #清楚屏幕
+        os.system('cls')   #清除屏幕
 except KeyboardInterrupt:
     print('end....')
 
-	
-	
-'''
-import pyautogui
-import os
-
-screenWidth, screenHeight = pyautogui.size()
-#pyautogui.moveTo(screenWidth / 2, screenHeight / 2)while(1)
-a=pyautogui.position()
-print(a)
-secs_between_keys = 0.1
-pyautogui.typewrite('Hello world!\n', interval=secs_between_keys)
-pyautogui.click(x=1013, y=521, clicks=1,  button='left')
-#print(b)
-#pyautogui.hotkey('ctrl', 'v') # 粘贴我是粘贴来的你好
-
-try:
-    while True:
-        x, y = pyautogui.position()
-        print(x,y)
-    except KeyboardInterrupt:
-        print(‘\nExit.’)
-		
-os.system("pause")
-'''
